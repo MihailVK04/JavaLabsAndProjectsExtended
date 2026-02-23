fe44af02ae1e8579376c8b1b50bebc7262c55136
MihailVK04 <149954001+MihailVK04@users.noreply.github.com>
2026-02-23 19:44:55 +0200

    Add files via upload

---
+++ Laboratory Exercise 3 - Burnout Prevention System/src/src/bg/sofia/uni/fmi/mjt/burnout/semester/AbstractSemesterPlanner.java
@@ -0,0 +1,117 @@
+package bg.sofia.uni.fmi.mjt.burnout.semester;
+
+import bg.sofia.uni.fmi.mjt.burnout.exception.DisappointmentException;
+import bg.sofia.uni.fmi.mjt.burnout.plan.SemesterPlan;
+import bg.sofia.uni.fmi.mjt.burnout.subject.Category;
+import bg.sofia.uni.fmi.mjt.burnout.subject.SubjectRequirement;
+import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
+
+public abstract sealed class AbstractSemesterPlanner implements SemesterPlannerAPI permits
+    ComputerScienceSemesterPlanner, SoftwareEngineeringSemesterPlanner {
+
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int DAYS_FOR_ONE_JAR = 5;
+    private static final int DOUBLE = 2;
+
+    private void checkConditionsForJarCount(UniversitySubject[] subjects, int maximumSlackTime, int semesterDuration) {
+        if ( subjects == null ) {
+            throw new IllegalArgumentException("Subjects are null");
+        } else if ( maximumSlackTime <= ZERO ) {
+            throw new IllegalArgumentException("MaximumSlackTime is not positive");
+        } else if ( semesterDuration <= ZERO ) {
+            throw new IllegalArgumentException("SemesterDuration is not positive");
+        }
+    }
+
+    protected int addSubjectsForCoveringCredits(UniversitySubject[] destination,
+                                                UniversitySubject[] arrayWithAllSubjects, int size, int credits) {
+        int maxCredits = ZERO;
+        UniversitySubject maxElement = null;
+
+        for ( UniversitySubject subject : arrayWithAllSubjects) {
+            if (subject.credits() > maxCredits) {
+                for (int i = ZERO; i <= size; i++) {
+                    if (!(subject.equals(destination[i]))) {
+                        maxCredits = subject.credits();
+                        maxElement = subject;
+                        break;
+                    }
+                }
+            }
+        }
+
+        destination[size + ONE] = maxElement;
+        return maxCredits + credits;
+    }
+
+    protected int concatUniversitySubjectsArrays(UniversitySubject[] destination, UniversitySubject[] arrayForConcat,
+                                                 int size) {
+        System.arraycopy(arrayForConcat, ZERO, destination, size, arrayForConcat.length);
+        return size + arrayForConcat.length - ONE;
+    }
+
+    protected boolean checkForNotEnoughSubjects(SemesterPlan semesterPlan) {
+
+        for (SubjectRequirement requirement : semesterPlan.subjectRequirements()) {
+            int count = ZERO;
+
+            for (UniversitySubject subject : semesterPlan.subjects()) {
+                if (subject.category().equals(requirement.category())) {
+                    count++;
+                }
+            }
+            if (count < requirement.minAmountEnrolled()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected boolean areUniqueSubjectRequirements(SubjectRequirement[] subjectRequirements) {
+        Category[] categoryValues = Category.values();
+        int[] occurrences = new int[categoryValues.length];
+
+        for (SubjectRequirement requirement : subjectRequirements) {
+            Category category = requirement.category();
+            int index = ZERO;
+
+            for (Category match : categoryValues) {
+                if (match.equals(category)) {
+                    occurrences[index] += ONE;
+                }
+                if (occurrences[index] > ONE) {
+                    return false;
+                }
+                index++;
+            }
+        }
+
+        return true;
+    }
+
+    public int calculateJarCount(UniversitySubject[] subjects, int maximumSlackTime, int semesterDuration) {
+
+        checkConditionsForJarCount(subjects, maximumSlackTime, semesterDuration);
+
+        int sumOfBreak = ZERO;
+        int sumOfLearning = ZERO;
+        for ( UniversitySubject subject : subjects ) {
+            sumOfBreak += (int) Math.ceil((subject.neededStudyTime() * subject.category().getCoefficient()));
+            sumOfLearning += subject.neededStudyTime();
+        }
+
+        int freeTimeThisSemester = semesterDuration - (sumOfBreak + sumOfLearning);
+        if (freeTimeThisSemester > maximumSlackTime) {
+            throw new DisappointmentException("Grandkid has too much free time");
+        }
+
+        int jarsPerDays = Math.ceilDiv(sumOfLearning, DAYS_FOR_ONE_JAR);
+        if (freeTimeThisSemester < ZERO) {
+            return jarsPerDays * DOUBLE;
+        } else {
+            return jarsPerDays;
+        }
+    }
+}
---
+++ Laboratory Exercise 3 - Burnout Prevention System/src/src/bg/sofia/uni/fmi/mjt/burnout/semester/SoftwareEngineeringSemesterPlanner.java
@@ -0,0 +1,78 @@
+package bg.sofia.uni.fmi.mjt.burnout.semester;
+
+import bg.sofia.uni.fmi.mjt.burnout.exception.CryToStudentsDepartmentException;
+import bg.sofia.uni.fmi.mjt.burnout.exception.InvalidSubjectRequirementsException;
+import bg.sofia.uni.fmi.mjt.burnout.plan.SemesterPlan;
+import bg.sofia.uni.fmi.mjt.burnout.subject.SubjectRequirement;
+import bg.sofia.uni.fmi.mjt.burnout.subject.UniversitySubject;
+
+public final class SoftwareEngineeringSemesterPlanner extends AbstractSemesterPlanner {
+
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+
+    private void checkSubjectListConditions(SemesterPlan semesterPlan) {
+        if (semesterPlan.subjects() == null) {
+            throw new IllegalArgumentException("Semester plan is missing or null");
+        }
+
+        if (!areUniqueSubjectRequirements(semesterPlan.subjectRequirements())) {
+            throw new InvalidSubjectRequirementsException("There are duplicated categories");
+        }
+
+        if (checkForNotEnoughSubjects(semesterPlan)) {
+            throw new CryToStudentsDepartmentException("Student cannot complete the requirements");
+        }
+    }
+
+    private UniversitySubject[] getSubjectForCategory(UniversitySubject[] subjects, SubjectRequirement requirement) {
+
+        UniversitySubject[] memory = new UniversitySubject[requirement.minAmountEnrolled()];
+        UniversitySubject maxElement;
+        int maxCredits = ZERO;
+
+        for (UniversitySubject subject : subjects) {
+            if (subject.category().equals(requirement.category())) {
+                if (subject.credits() > maxCredits) {
+                    maxCredits = subject.credits();
+                    maxElement = subject;
+                    for (int i = ZERO; i < requirement.minAmountEnrolled() - ONE; i++) {
+                        memory[i] = memory[i + ONE];
+                    }
+                    memory[requirement.minAmountEnrolled() - ONE] = maxElement;
+                }
+            }
+        }
+
+        return memory;
+    }
+
+    public UniversitySubject[] calculateSubjectList(SemesterPlan semesterPlan) throws
+        InvalidSubjectRequirementsException {
+
+        checkSubjectListConditions(semesterPlan);
+
+        UniversitySubject[] result = new UniversitySubject[semesterPlan.subjects().length];
+        int size = ZERO;
+        for (SubjectRequirement requirement : semesterPlan.subjectRequirements()) {
+            size = concatUniversitySubjectsArrays(result, getSubjectForCategory(semesterPlan.subjects(), requirement),
+                size);
+        }
+
+        int credits = ZERO;
+        for ( int i = ZERO; i <= size; i++) {
+            credits += result[i].credits();
+        }
+
+        for ( int i = size; i <= (semesterPlan.subjects().length - ONE); i++) {
+            if ( credits >= semesterPlan.minimalAmountOfCredits()) {
+                return result;
+            }
+            credits = addSubjectsForCoveringCredits(result, semesterPlan.subjects(), i, credits);
+        }
+        if ( credits < semesterPlan.minimalAmountOfCredits()) {
+            throw new CryToStudentsDepartmentException("Student cant reach credit limit with the subjects");
+        }
+        return result;
+    }
+}
