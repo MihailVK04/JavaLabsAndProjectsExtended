066dc841b801890f55d9ae790b669243e2a04c0c
Mihail <mihail5212@abv.bg>
2026-05-02 03:06:43 +0300

    Week 8 - added two tables and some repository and entity classes

---
+++ theatre-ticketing-system/week08/theatre-system-week8/src/main/java/bg/uni/fmi/theatre/repository/adapters/HallRepositoryAdapter.java
@@ -0,0 +1,43 @@
+package bg.uni.fmi.theatre.repository.adapters;
+
+import bg.uni.fmi.theatre.domain.Hall;
+import bg.uni.fmi.theatre.entity.HallEntity;
+import bg.uni.fmi.theatre.repository.HallJpaRepository;
+import bg.uni.fmi.theatre.repository.HallRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class HallRepositoryAdapter implements HallRepository {
+
+    private final HallJpaRepository hallJpaRepository;
+
+    public HallRepositoryAdapter(HallJpaRepository hallJpaRepository) {
+        this.hallJpaRepository = hallJpaRepository;
+    }
+
+    @Override
+    public Hall save(Hall hall) {
+        HallEntity saved = hallJpaRepository.save(HallEntity.from(hall));
+        return saved.toHall();
+    }
+
+    @Override
+    public Optional<Hall> findById(Long id) {
+        return hallJpaRepository.findById(id).map(HallEntity::toHall);
+    }
+
+    @Override
+    public List<Hall> findAll() {
+        return hallJpaRepository.findAll().stream().
+            map(HallEntity::toHall)
+            .toList();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        hallJpaRepository.deleteById(id);
+    }
+}
